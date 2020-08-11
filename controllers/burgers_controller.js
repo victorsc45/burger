b3501909069bf0624eb2225bce5453661339aed4
victorsc45 <victorlitzau@gmail.com>
2020-08-11 16:01:34 -0400

    controller with delete added completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,17 +10,39 @@ router.get('/', (req, res) => {
         };
         console.log(hdbrsObj);
         res.render("index", hdbrsObj);
-    })
-});
-router.post('/api/burgers', (req, res) => {
-    burger.insertOne(
-        ['burger_name', 'devoured'],
-        [req.body.burger_name, req.body.devoured],
-        function (result) {
-            res.json({ id: result.insertId });
-        }
-    )
-})
-router.put('/api/burgers/:id', (req, res)[
+    });
 
-])
+    router.post('/api/burgers', (req, res) => {
+        burger.insertOne(
+            ['burger_name', 'devoured'],
+            [req.body.burger_name, req.body.devoured],
+            function (result) {
+                res.json({ id: result.insertId });
+            });
+    });
+    router.put('/api/burgers/:id', (req, res) => {
+        let condition = "id = " + req.params.id;
+        console.log('condition', condition);
+        burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+            if (result, changedRows === 0) {
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end();
+            }
+        });
+    });
+    router.deleteOne(condition, (req, res) => {
+        let condition = "id = " + req.params.id;
+        console.log("condition", condition);
+        burger.deleteOne(condition, function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end();
+            }
+        });
+    });
+
+});
