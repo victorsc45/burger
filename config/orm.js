59807345ee58229f6a58fc6d8d6129c217aac29b
victorsc45 <victorlitzau@gmail.com>
2020-08-13 14:44:48 -0400

    commiting documentation and code clean

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,5 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
-
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
@@ -8,18 +7,14 @@ const connection = require("../config/connection.js");
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   let arr = [];
-
   for (let i = 0; i < num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   let arr = [];
-
   // loop through the keys and push the key/value as a string int arr
   for (let key in ob) {
     let value = ob[key];
@@ -34,7 +29,6 @@ function objToSql(ob) {
       arr.push(key + "=" + value);
     }
   }
-
   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
@@ -54,16 +48,12 @@ let orm = {
   // ORM insert query for adding a burger
   insertOne: (table, cols, vals, cb) => {
     let query = "INSERT INTO " + table;
-
     query += " (";
     query += cols.toString();
     query += ") ";
     query += "VALUES (";
     query += printQuestionMarks(vals.length);
     query += ") ";
-
-    console.log(query);
-
     connection.query(query, vals, (err, result) => {
       if (err) {
         throw err;
@@ -75,13 +65,10 @@ let orm = {
   // update a burger to devoured
   updateOne: (table, objColVals, condition, cb) => {
     let query = "UPDATE " + table;
-
     query += " SET ";
     query += objToSql(objColVals);
     query += " WHERE ";
     query += condition;
-
-    console.log(query);
     connection.query(query, (err, result) => {
       if (err) {
         throw err;
@@ -95,12 +82,10 @@ let orm = {
     var queryString = "DELETE FROM " + table;
     queryString += " WHERE ";
     queryString += condition;
-
     connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   }
