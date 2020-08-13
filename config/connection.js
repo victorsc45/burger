3f08afbb49ef8cf4ba3a23c9f1b188c78c968c46
victorsc45 <victorlitzau@gmail.com>
2020-08-13 15:35:51 -0400

    added heroku db addon for deployment

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,21 @@ const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 //process environment encapsulated for privacy
 //sql connection creation
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  // port local is 3306
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+const connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    // port local is 3306
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
 
+  });
+};
 // Make connection.
 connection.connect((err) => {
   if (err) {
